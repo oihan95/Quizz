c5f5b2da68462f6ba0621ae60081b44e4c1b9070
Oihan <oihan@MacBook-Pro-de-Oihan.local>
2017-01-08 19:01:47 +0100

    Lab2: Erabiltzaileak paula batean agertzea

--- ShowUsersWithImage.php
+++ ShowUsersWithImage.php
@@ -5,23 +5,57 @@ $esteka = mysqli_connect("localhost", "root",  "", "Quiz");
 
 $ema = mysqli_query($esteka, "select * from Erabiltzaile");
 
-echo '<table border=1> <tr> <th> Izena </th>
-		<th> Lehenengo abizena </th>
-		<th> Bigarren abizena </th>
-		<th> Eposta </th>
-		<th> Pasahitza </th>	
-		<th> Telefono zenbakia </th>
-		<th> Espezialitatea </th>
-		<th> Espe_beste </th>
-		<th> Interesak </th>
-		<th> Argazkia </th>
-		</tr>';
-
-while( $row=mysqli_fetch_array($ema, MYSQLI_ASSOC)) {
-	echo '<tr> <td>'.$row['Izena'].'</td> <td>'.$row['Abizena1'].'</td> <td>'.$row['Abizena2'].'</td> <td>'.$row['Eposta'].'</td> <td>'.$row['Pasahitza'].'</td> <td>'.$row['Telefono zenbakia'].'</td> <td>'.$row['Espezialitatea'].'</td> <td>'.$row['Espe_beste'].'</td> <td>'.$row['Interesak'].'</td> <td><img src="img/irudiak/'.$row['Argazkia'].'"  style="width:304px;"/></td> </tr>';
-}
-
-mysqli_free_result($ema);
-mysqli_close($esteka);
-
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+    	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<title>Quizzes - Erabiltzaileak</title>
+    	<link rel='stylesheet' type='text/css' href='stylesPWS/style.css' />
+    	<link rel="stylesheet" type="text/css" href="stylesPWS/table.css">
+  	</head>
+  	<body>
+	  	<div class = "header">
+			<div id="logo">Quiz: crazy questions</div>
+				<div class="navbar">
+					<a href="layout.html">Home</a>
+					<a href='#'>Quizzes</a>
+					<a href="signup.html">Sign Up</a>
+					<a href="signin.html">Sign In</a>
+					<a href="credits.html">Credits</a>
+				</div>
+		</div>
+		<div class="wrapper">
+
+			<?php
+				echo('<div class="table">');
+				echo('<div class="header-row row">');
+				echo('<span class="cell"></span>');
+				echo('<span class="cell">Izena</span>');
+				echo('<span class="cell">Lehenengo abizena</span>');
+				echo('<span class="cell">Bigarren abizena</span>');
+				echo('<span class="cell">Eposta</span>');
+				echo('<span class="cell">Espezialitatea</span>');
+				echo('</div>');
+
+			while( $row=mysqli_fetch_array($ema, MYSQLI_ASSOC)) {
+				echo('<div class="row">');
+					echo('<span class="cell">'."<img src='img/irudiak/".$row['Argazkia']."'"." width="."150".">".'</span>');
+					echo('<span class="cell">'.$row['Izena'].'</span>');
+				    echo('<span class="cell">'.$row['Abizena1'].'</span>');
+				    echo('<span class="cell">'.$row['Abizena2'].'</span>');
+					echo('<span class="cell">'.$row['Eposta'].'</span>');
+					echo('<span class="cell">'.$row['Espezialitatea'].'</span>');
+					echo('</div>');
+					}
+			echo('</div>');
+
+			mysqli_free_result($ema);
+			mysqli_close($esteka);
+
+			?>
+
+		</div>
+	</body>
+</html>
