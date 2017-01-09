087b7ed73129f56d4a6856afe7b6dc45b30cd0ee
Oihan <oihan@MacBook-Pro-de-Oihan.local>
2017-01-10 00:13:19 +0100

    xml fitxategiko galderak bistaratzeko html plantilla

---
+++ seeXMLQuenstions.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+	<head>
+    	<meta http-equiv="content-type" content="text/html; charset=UTF-8">
+		<title>Quizzes - Galderak</title>
+    	<link rel='stylesheet' type='text/css' href='stylesPWS/style.css' />
+    	<link rel="stylesheet" type="text/css" href="stylesPWS/table.css">
+
+  	</head>
+  	<body>
+	  	<div class = "header">
+			<div id="logo">Quiz: crazy questions</div>
+				<div class="navbar">
+					<a href="layout.html">Home</a>
+					<a href='quizzes.php'>Quizzes</a>
+					<a href="signup.html">Sign Up</a>
+					<a href="signin.html">Sign In</a>
+					<a href="credits.html">Credits</a>
+				</div>
+		</div>
+		<div class="wrapper jump center">
+			<h4>Hauek dira Quizzes-eko XML fitxategian dauden galdera guztiak</h4>
+			<br>
+			<?php
+			
+			?>
+		</div>
+	</body>
+</html>
+
