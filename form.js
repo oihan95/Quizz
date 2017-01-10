43b36a6278e8943c3b162e2e240c072b7fbc9a33
Oihan <oihan@MacBook-Pro-de-Oihan.local>
2017-01-10 11:44:19 +0100

    erabiltzailearen informazioa lortzeko kodea (hobetzeke)

--- form.js
+++ form.js
@@ -103,3 +103,21 @@ function balidatulogin(){
 	}
 	return true;
 }
+
+function osatu(){
+	var dago=false;
+	var xmlDoc = document.getElementById('datuak').contentDocument;
+	var kor = document.getElementById("korreoa").value;
+	var epostenZer=xmlDoc.getElementsByTagName("eposta");
+	var tfnoZer=xmlDoc.getElementsByTagName("telefonoa");
+	for (var i = 0; i < epostenZer.length; i++) {
+		if (kor==epostenZer[i].childNodes[0].nodeValue){
+			document.getElementById('tfonoa').value=tfnoZer[i].childNodes[0].nodeValue;
+			dago=true;
+			break;
+		}
+	}
+	if (!dago){
+		alert ('Eposta hau ez dago UPV/EHUn erregistraturik. Sar ezazu beste bat');
+	}
+}
